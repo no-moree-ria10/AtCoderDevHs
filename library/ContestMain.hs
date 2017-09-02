2331c992b137339a92d336e31e9b993685ff0f1d
ria10 <otatakuma0714@gmail.com>
2017-09-02 16:29:50 +0900

    shap contestMain

--- library/ContestMain.hs
+++ library/ContestMain.hs
@@ -1,19 +1,83 @@
 -- extension section
 
 
--- import section
+
+module ContestMain where
+
+-- @@@@@@@@@@@@@@@@@--
+-- import section   --
+-- @@@@@@@@@@@@@@@@@--
+--    import A.B.C( function name )  ...
+-- or import A.B.C 
+
+
+
+-- bytestring
+
+
+-- containers
+
+
+-- deepseq
+
+
+-- regex-posix
+
+
+-- text
+
+
+-- unordered-containers
+
+
+-- vectors
+
+
+
+-- @@@@@@@@@@@@@@@@@--
+-- main section     --
+-- @@@@@@@@@@@@@@@@@--
+
+
+main :: IO ()
+main = do
+  input <- getInput
+  putStr . show . getAnswer $ input 
+
+-- main :: IO()
+-- main = do
+--   putStr $ show getInputAndAnser
+
+
+-- @@@@@@@@@@@@@@@@@@@@@--
+-- I/O DataType section --
+-- @@@@@@@@@@@@@@@@@@@@@--
+data Input = I (Int,[Int])
+  deriving(Show)
+
+data Answer = Yes | No
+
+instance Show Answer where
+  show = undefined
+
+
+-- @@@@@@@@@@@@@@@@@--
+-- answerSection    --
+-- @@@@@@@@@@@@@@@@@--
+getInput :: IO Input
+getInput = undefined
+
+getAnswer :: Input -> Answer 
+getAnswer = undefined
+
+-- @@@@@@@@@@@@@@@@@@@@@@@@@@--
+-- question uniqu section    --
+-- ( need to self write)     --
+-- @@@@@@@@@@@@@@@@@@@@@@@@@@--
 
 
 
 
--- main section
-
-
--- I/O DataType section
-
-
--- answerSection
-
-
-
--- util section
+-- @@@@@@@@@@@@@@@@@--
+-- util section     --
+-- @@@@@@@@@@@@@@@@@--
