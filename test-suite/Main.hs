a771b5f00273dca79c6f45028cae169d5ea98977
ria10 <otatakuma0714@gmail.com>
2017-09-02 16:54:01 +0900

    ad answerTest1

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -6,12 +6,27 @@ import qualified Test.Tasty
 -- writing tests. Its website has more info: <https://hspec.github.io>.
 import Test.Tasty.Hspec
 
+
+import qualified ContestMain as CM
+
 main :: IO ()
 main = do
     test <- testSpec "AtCoderDevHs" spec
     Test.Tasty.defaultMain test
 
+
 spec :: Spec
 spec = parallel $ do
-    it "is trivially true" $ do
-        True `shouldBe` True
+    it "AnswerTest1" $ do
+        (CM.getAnswer testInp1) `shouldBe` testAns1
+
+
+
+
+
+
+testInp1 :: CM.Input
+testInp1 = undefined
+
+testAns1 :: CM.Answer
+testAns1 = undefined
